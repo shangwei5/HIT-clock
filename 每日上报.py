98f41403c045ee2728ecf8fbbe1a165c391fa9c1
Ming Liu <lm.945@foxmail.com>
2021-06-25 10:25:08 +0800

    Update 每日上报.py

--- 每日上报.py
+++ 每日上报.py
@@ -24,12 +24,14 @@ driver.find_element_by_id('load').click()
 
 # VPN login
 driver.get('http://xg-hit-edu-cn-s.ivpn.hit.edu.cn/zhxy-xgzs/xg_mobile/xs/yqxx')
+print(driver.current_url)
 driver.find_element_by_id('mobileUsername').send_keys(USERNAME)
 driver.find_element_by_id('mobilePassword').send_keys(PASSWORD)
 driver.find_element_by_id('load').click()
 
 # http://xg-hit-edu-cn-s.ivpn.hit.edu.cn
 driver.get('http://xg-hit-edu-cn-s.ivpn.hit.edu.cn/zhxy-xgzs/xg_mobile/xs/yqxx')
+print(driver.current_url)
 
 driver.find_element_by_class_name('right_btn').click()
 sleep(1)
